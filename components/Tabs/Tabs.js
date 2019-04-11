c6fd76d625ca69a487db44c3f0aeb44b30cb0d7d
ken1286 <kindle1286@gmail.com>
2019-04-11 15:25:18 -0700

    TabLink class fixed

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -6,18 +6,19 @@ class TabLink {
     this.element = element;
     // Get the custom data attribute on the Link
     // this.data;
-    this.data = this.element.dataset;
+    this.data = this.element.dataset.tab;
     // console.log(this.data);
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
-    this.itemElement = `"${this.data.tab}"`;
+    this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+    // console.log(this.itemElement);
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
     this.tabItem = new TabItem(this.itemElement);
     // console.log(this.tabItem);
     // Add a click event listener on this instance, calling the select method on click
     this.element.addEventListener("click", () => {this.select()});
-  };
+  }
 
   select() {
     // Get all of the elements with the tabs-link class
@@ -25,14 +26,14 @@ class TabLink {
     const links = document.getElementsByClassName(".tabs-link");
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
-    Array.from(links).forEach( link => {
-      link.classList.toggle("tabs-link-selected");
+    Array.from(links).forEach( currentElement => {
+      currentElement.classList.remove("tabs-link-selected");
     });
     // Add a class named "tabs-link-selected" to this link
     // this.element;
     this.element.classList.add("tabs-link-selected");
     // Call the select method on the item associated with this link
-    this.itemElement.select();
+    this.select();
   }
 }
 
@@ -49,7 +50,7 @@ class TabItem {
     const items = document.querySelectorAll(".tabs-item");
     // Remove the class "tabs-item-selected" from each element
     items.forEach( item => {
-      item.classList.toggle("tabs-item-selected");
+      item.classList.remove("tabs-item-selected");
     });
     // Add a class named "tabs-item-selected" to this element
     //this.element;
