57f9dca45e192d54d6df09cf23e2c433334a2217
ken1286 <kindle1286@gmail.com>
2019-04-11 15:44:14 -0700

    TabItem class and method made

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -23,7 +23,7 @@ class TabLink {
   select() {
     // Get all of the elements with the tabs-link class
     // const links;
-    const links = document.getElementsByClassName(".tabs-link");
+    const links = document.querySelectorAll(".tabs-link");
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
     Array.from(links).forEach( currentElement => {
@@ -57,6 +57,7 @@ class TabItem {
     this.element.classList.add("tabs-item-selected");
   }
 }
+
 
 /* START HERE: 
 
